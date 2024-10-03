0c56f0b20534d49363299556d772d6b4241646fd
Mert-Ozbolat <mertozbolat008@gmail.com>
2024-10-03 13:08:17 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,69 @@
+const API_URL =
+  "https://api.themoviedb.org/3/movie/popular?api_key=a073e0a2000c91fe2ac37b04fd193338";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?api_key=a073e0a2000c91fe2ac37b04fd193338&query=";
+
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const main = document.getElementById("main");
+
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  console.log(data.results);
+  showMovies(data.results);
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value.trim();
+  if (searchTerm && searchTerm !== "") {
+    const searchUrl = SEARCH_API + encodeURIComponent(searchTerm);
+    getMovies(searchUrl);
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
+
+function showMovies(movies) {
+  main.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieElement = document.createElement("div");
+    movieElement.classList.add("movie");
+
+    movieElement.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(
+                  vote_average
+                )}">${vote_average.toFixed(1)}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+            `;
+
+    main.appendChild(movieElement);
+  });
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Moview🎞
+
+This project is a film search web page similar to IMDb, created using HTML, CSS, and JavaScript. Users can search for their favorite movies, read descriptions, and view their ratings.
+
+# Features📽🎟
+
+- **Movie Descriptions:** A brief description is displayed for each movie.
+- **Movie Search:** Users can quickly search for movie titles to access detailed information.
+- **Movie Ratings:** The rating of each movie is shown.
+- **Responsive Design:** The page is optimized for different device sizes.
+
+# Preview📺
+
+![](./MovieWeb.gif)
+# Moview-Web
